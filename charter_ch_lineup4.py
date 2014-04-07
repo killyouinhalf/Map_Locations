d8f3f154f7514aacfc641c39d8129e5c94262595
killyouinhalf <klabeau@gmail.com>
2014-04-07 12:41:34 -0400

    It works! Now just to fine tune it

--- charter_ch_lineup4.py
+++ charter_ch_lineup4.py
@@ -5,93 +5,113 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.support.ui import WebDriverWait
 from pygeocoder import Geocoder
 import pygmaps
-import gmaps2
 
-# Open Firefox to charter using Selenium
-browser = Firefox()
-browser.get('https://www.charter.com/browse/content/new-channel-lineup')
+class map_charter:
+    def open_browser(self):
+        # Open Firefox to charter using Selenium
+        browser = Firefox()
+        browser.get('https://www.charter.com/browse/content/new-channel-lineup')
 
-# Click the "choose state" menu using Selenium
-wait = WebDriverWait('browser', 20)
-button = browser.find_element_by_css_selector('.drop-down-current')
-button.click()
+        # Click the "choose state" menu using Selenium
+        wait = WebDriverWait('browser', 20)
+        button = browser.find_element_by_css_selector('.drop-down-current')
+        button.click()
 
-# # Identify all states in the list, read as text using Selenium
-list_item = browser.find_element_by_class_name('drop-down-list')
-states = list_item.text
+        # # Identify all states in the list, read as text using Selenium
+        list_item = browser.find_element_by_class_name('drop-down-list')
+        states = list_item.text
+        # print states
 
-# sel_st = raw_input('Type in 2 letter st abbreviation: ')
-find_st = browser.find_element_by_link_text('MI')
-find_st.click()
-sleep(1)
-list_region = browser.find_element_by_xpath('//*[@id="select-region"]/div[2]') # find and click Choose Region button
-list_region.click()
-cur_state = browser.find_element_by_xpath('//*[@id="select-region"]/div[3]')
+        # sel_st = raw_input('Type in 2 letter st abbreviation: ')
+        self.find_st = browser.find_element_by_link_text('AL')
+        # return find_st
+        self.sel_st = self.find_st.text + ', USA'
+        print self.sel_st
+        self.find_st.click()
+        sleep(1)
+        list_region = browser.find_element_by_xpath('//*[@id="select-region"]/div[2]') # find and click Choose Region button
+        list_region.click()
+        cur_state = browser.find_element_by_xpath('//*[@id="select-region"]/div[3]')
 
-# text_region = cur_state.find_elements_by_tag_name('a') # find all regions by <a> tag
-l_regions = cur_state.text
+        # text_region = cur_state.find_elements_by_tag_name('a') # find all regions by <a> tag
+        l_regions = cur_state.text
 
-print "l_regions"
-print l_regions
-# creates text file with all the region
-regions = open('Regions.text', 'w+')
-regions.write(l_regions)
-sleep(3.0)
+        print "l_regions: "
+        print l_regions
+        # creates text file with all the region
+        regions = open('Regions.text', 'w+')
+        # regions.write("Come on!" '\n')
+        regions.write(l_regions)
+        sleep(3.0)
+        regions.close()
+        browser.close()
 
+    # open_browser()
 
-# Place region points on map
-# gmaps2.gmaps_mapit(l_regions)
+    def gmaps_mapit(self):
+        print ("GO!")
+        # f = open('Regions.text', 'r')
+        # map_regions = []
+        # for line in f:
+        #     map_regions.append(line.rstrip('\n'))
 
-def gmaps_mapit():
-    # f = open('Regions.text', 'r')
-    # map_regions = []
-    # for line in f:
-    #     map_regions.append(line.rstrip('\n'))
+        geo = Geocoder.geocode(self.sel_st)
+        geo_st = (geo[0].coordinates)
+        st_joint = ', '.join(map(str, geo_st))
+        split_st_joint = st_joint.split(',', 2)
+        st_lat = split_st_joint[0]
+        st_lon = split_st_joint[1]
+        print st_joint
+        print split_st_joint
+        print st_lat
+        print st_lon
+        regions_map = pygmaps.maps(float(st_lat), float(st_lon),7) # Generate state map
+        f = open('Regions.text', 'r')
+        map_regions = []
+        for line in f:
+            map_regions.append(line.rstrip('\n'))
+        print "map_regions: "
+        print map_regions
 
-    # print state
-    regions_map = pygmaps.maps(44.3148443, -85.60236429999999,7) # Generate state map
-    f = open('Regions.text', 'r')
-    map_regions = []
-    for line in f:
-        map_regions.append(line.rstrip('\n'))
-    print "map_regions"
-    print map_regions
-
-    def geo_region():
-        for item in map_regions:
-            try:
-                geo = Geocoder.geocode(item)
-            except:
-                pass
-            else:
-                point = (geo[0].coordinates)
-                title = item
-                joint = ', '.join(map(str, point))
-                split_point = joint.split(',', 2)
-                lat = split_point[0]
-                lon = split_point[1]
-                if '(All Digital)' in item:
-                    regions_map.addpoint(float(lat), float(lon), "#0101DF", title=title)
+        def geo_region():
+            for item in map_regions:
+                try:
+                    geo = Geocoder.geocode(item)
+                except:
+                    pass
                 else:
-                    regions_map.addpoint(float(lat), float(lon), "#FF0000", title=title)
+                    point = (geo[0].coordinates)
+                    title = item
+                    joint = ', '.join(map(str, point))
+                    split_point = joint.split(',', 2)
+                    lat = split_point[0]
+                    lon = split_point[1]
+                    if '(All Digital)' in item:
+                        regions_map.addpoint(float(lat), float(lon), "#0101DF", title=title)
+                    else:
+                        regions_map.addpoint(float(lat), float(lon), "#FF0000", title=title)
 
-            # print "Point: "
-            # print point
-            # print "Joint: "
-            # print joint
-            # print "split_joint: "
-            # print split_point
+                # print "Point: "
+                # print point
+                # print "Joint: "
+                # print joint
+                # print "split_joint: "
+                # print split_point
 
-            sleep(0.1)
+                sleep(0.1)
 
-    geo_region()
+        geo_region()
 
-    regions_map.draw('./mymap.html')
-    print "Done"
+        regions_map.draw('./mymap.html')
+        print "Done"
 
-gmaps_mapit()
+    # gmaps_mapit()
 
-# url = './mymap.html'
-# webbrowser.open_new_tab(url)
+go = map_charter()
+go.open_browser()
+go.gmaps_mapit()
 
-browser.close()
+    # url = './mymap.html'
+    # webbrowser.open_new_tab(url)
+
+    # browser.close()
--- gmaps2.py
+++ gmaps2.py
@@ -5,47 +5,44 @@ import pygmaps
 import time
 
 # generate list of all regions
-# def gmaps_mapit():
-#     regions_map = pygmaps.maps(44.3148443, -85.60236429999999,7) # Generate state map
-#     f = open('Regions2.text', 'r')
-#     map_regions = []
-#     for line in f:
-#         map_regions.append(line.rstrip('\n'))
-#     print map_regions
-#
-#     def geo_region():
-#         for item in map_regions:
-#             try:
-#                 geo = Geocoder.geocode(item)
-#             except:
-#                 pass
-#             else:
-#                 point = (geo[0].coordinates)
-#                 title = item
-#                 joint = ', '.join(map(str, point))
-#                 split_point = joint.split(',', 2)
-#                 lat = split_point[0]
-#                 lon = split_point[1]
-#                 if '(All Digital)' in item:
-#                     regions_map.addpoint(float(lat), float(lon), "#0101DF", title=title)
-#                 else:
-#                     regions_map.addpoint(float(lat), float(lon), "#FF0000", title=title)
-#
-#             # print "Point: "
-#             # print point
-#             # print "Joint: "
-#             # print joint
-#             # print "split_joint: "
-#             # print split_point
-#
-#             time.sleep(0.1)
-#
-#     geo_region()
-#
-#     regions_map.draw('./mymap.html')
-#
-# gmaps_mapit()
+def gmaps_mapit():
+    regions_map = pygmaps.maps(44.3148443, -85.60236429999999,7) # Generate state map
+    f = open('Regions2.text', 'r')
+    map_regions = []
+    for line in f:
+        map_regions.append(line.rstrip('\n'))
+    print map_regions
+
+    def geo_region():
+        for item in map_regions:
+            try:
+                geo = Geocoder.geocode(item)
+            except:
+                pass
+            else:
+                point = (geo[0].coordinates)
+                title = item
+                joint = ', '.join(map(str, point))
+                split_point = joint.split(',', 2)
+                lat = split_point[0]
+                lon = split_point[1]
+                if '(All Digital)' in item:
+                    regions_map.addpoint(float(lat), float(lon), "#0101DF", title=title)
+                else:
+                    regions_map.addpoint(float(lat), float(lon), "#FF0000", title=title)
+                time.sleep(0.1)
+
+            # print "Point: "
+            # print point
+            # print "Joint: "
+            # print joint
+            # print "split_joint: "
+            # print split_point
 
 
 
+    geo_region()
 
+    regions_map.draw('./mymap.html')
+
+gmaps_mapit()
